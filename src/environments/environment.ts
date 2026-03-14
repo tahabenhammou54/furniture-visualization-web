135fa125d7d6b197e32ddc628b20432b4e3a8495
TahaBenHammou <103946547+TahaBenHammou@users.noreply.github.com>
2026-03-14 13:19:38 +0000

    feat: update API URL to production endpoint in environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://furniture-visualization-api-production.up.railway.app/api'
+  // apiUrl: 'http://localhost:3000/api',
   paypalClientId: 'AWDBzuXCQaMm33vwogzNEMdi8WHdY3HIl1Oguo4G-eo8oCFCDiZD54GYsmh3xVaG7obnGLVL9tGkQ-AI',
 };
 
