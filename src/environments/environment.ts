23edf8d711f43d0707b78189ceff7f7012b60991
TahaBenHammou <103946547+TahaBenHammou@users.noreply.github.com>
2026-03-14 13:29:18 +0000

    fix: add missing comma in API URL declaration in environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://furniture-visualization-api-production.up.railway.app/api'
+  apiUrl: 'https://furniture-visualization-api-production.up.railway.app/api',
   // apiUrl: 'http://localhost:3000/api',
   paypalClientId: 'AWDBzuXCQaMm33vwogzNEMdi8WHdY3HIl1Oguo4G-eo8oCFCDiZD54GYsmh3xVaG7obnGLVL9tGkQ-AI',
 };
