1a2624b3c27242611d1c64f5c08e13e30995c5bc
TahaBenHammou <tahabenhammou54@gmail.com>
2026-03-28 20:59:03 +0000

    paywal update

--- src/app/interceptors/paywall.interceptor.ts
+++ src/app/interceptors/paywall.interceptor.ts
@@ -1,22 +1,17 @@
 import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { Router } from '@angular/router';
-import { NavController } from '@ionic/angular/standalone';
 import { AuthService } from '../services/auth.service';
+import { DOCUMENT } from '@angular/common';
 import { catchError, throwError } from 'rxjs';
 
 export const paywallInterceptor: HttpInterceptorFn = (req, next) => {
-  const router = inject(Router);
-  const navCtrl = inject(NavController);
   const auth = inject(AuthService);
+  const doc = inject(DOCUMENT);
 
   return next(req).pipe(
     catchError((err: HttpErrorResponse) => {
       if (err.status === 402 && auth.token) {
-        const currentUrl = router.url;
-        navCtrl.navigateForward('/tabs/subscription', {
-          state: { from: currentUrl },
-        });
+        doc.defaultView!.location.href = '/tabs/subscription';
       }
       return throwError(() => err);
     }),
